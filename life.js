44a67e4dc626f8e5bffd41044fa849fa46621f31
Barnabas <105345908+banx4real@users.noreply.github.com>
2022-06-24 12:44:23 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,11 @@
+alert('My name is Barnabas');
+alert('5.8ft');
+alert('Nigeria');
+
+var name = ('My name is Barnabas Ojo');
+var height = ('5.8ft');
+var country = ('Nigeria');
+
+console.log(name);
+console.log(height);
+console.log(country);
